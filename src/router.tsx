d859aacdc4134dcddb315bb5058def2dd641ce16
Vasili Karpenkin <v.karpenkin@gmail.com>
2023-08-22 23:42:12 +0300

    0.1.7 Routing

---
+++ src/router.tsx
@@ -0,0 +1,39 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  RouteObject,
+  Outlet,
+  Navigate,
+} from 'react-router-dom'
+
+const publicRoutes: RouteObject[] = [
+  {
+    path: '/login',
+    element: <div>login</div>,
+  },
+]
+
+const privateRoutes: RouteObject[] = [
+  {
+    path: '/',
+    element: <div>hello</div>,
+  },
+]
+
+const router = createBrowserRouter([
+  {
+    element: <PrivateRoutes />,
+    children: privateRoutes,
+  },
+  ...publicRoutes,
+])
+
+export const Router = () => {
+  return <RouterProvider router={router} />
+}
+
+function PrivateRoutes() {
+  const isAuthenticated = false
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
