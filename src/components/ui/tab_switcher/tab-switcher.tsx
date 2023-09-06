0155083f2d4a250b212fc2068e30ca4f937e4a45
Vasili Karpenkin <v.karpenkin@gmail.com>
2023-09-06 22:20:38 +0300

    0.2.7 TabSwitcher

--- src/components/ui/tab_switcher/tab-switcher.tsx
+++ src/components/ui/tab_switcher/tab-switcher.tsx
@@ -1,46 +1,73 @@
 import { FC } from 'react'
 
-import * as ToggleGroup from '@radix-ui/react-toggle-group'
-import { ToggleGroupSingleProps } from '@radix-ui/react-toggle-group'
 import styled, { css } from 'styled-components'
 
 import { Typography } from '@/components'
 
 export type TabSwitcherPropsType = {
+  value?: string
+  onClick(value: string): void
   array: string[]
-} & ToggleGroupSingleProps
-export const TabSwitcher: FC<TabSwitcherPropsType> = ({ array, value, ...rest }) => {
-  console.log(value)
+  disabled?: boolean
+}
+export const TabSwitcher: FC<TabSwitcherPropsType> = props => {
+  const { array, value, onClick, disabled } = props
 
   return (
-    <ToggleGroupRoot value={value} {...rest}>
+    <ToggleGroup>
       {array.map(i => (
-        <ToggleGroupItem key={i} value={i} active={i === value ? 'true' : 'false'}>
-          <Typography as={'body1'}>{i}</Typography>
-        </ToggleGroupItem>
+        <ToggleItem
+          key={i}
+          value={i}
+          disabled={disabled}
+          onClick={() => onClick(i)}
+          active={i === value ? 'true' : 'false'}
+        >
+          <Typography className={'Text'} as={'body1'}>
+            {i}
+          </Typography>
+        </ToggleItem>
       ))}
-    </ToggleGroupRoot>
+    </ToggleGroup>
   )
 }
+type StyledProps = Omit<TabSwitcherPropsType, 'array'> & { active: string }
 
-const ToggleGroupRoot = styled(ToggleGroup.Root)`
+const ToggleGroup = styled.div`
   display: inline-flex;
   background-color: #4c4c4c;
   padding: 1px;
   gap: 1px;
 `
 
-const ToggleGroupItem = styled(ToggleGroup.Item)<{ value: string; active: string }>`
-  display: flex;
-  padding: 6px 24px;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  ${props =>
-    props.active === 'true' &&
+const ToggleItem = styled.span<StyledProps>(props => {
+  const { active, disabled, theme } = props
+
+  return css`
+    display: flex;
+    padding: 6px 24px;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: 0.3s;
+    ${active === 'true' &&
     css`
-      box-shadow: 0 0 0 1px ${props => props.theme.colors.accent_500};
+      box-shadow: 0 0 0 1px ${theme.colors.accent_500};
     `};
-  background: ${props => (props.active === 'true' ? props.theme.colors.accent_500 : 'black')};
-  border: none;
-`
+    background: ${active === 'true' ? theme.colors.accent_500 : 'black'};
+    border: none;
+    user-select: none;
+    &:hover {
+      background: ${active === 'false' && !disabled && theme.colors.dark_300};
+    }
+    ${disabled &&
+    css`
+      background: ${theme.colors.dark_700};
+      box-shadow: none;
+      cursor: default;
+      .Text {
+        color: ${theme.colors.dark_300};
+      }
+    `}
+  `
+})
