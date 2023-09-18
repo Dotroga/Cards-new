5587715297dc659d5d3c9463f59e664ff8d5ead1
Vasili Karpenkin <v.karpenkin@gmail.com>
2023-09-18 21:32:36 +0300

    0.3.1.2  Old Refactor

--- src/pages/decks/pagination/pagination.tsx
+++ src/pages/decks/pagination/pagination.tsx
@@ -3,17 +3,24 @@ import styled from 'styled-components'
 import { MyPagination, Typography } from '@/components'
 import { Select } from '@/components/ui/select'
 import { Pagination } from '@/services/decks'
+import { decksActions } from '@/services/decks/decks.slice.ts'
 import { useAppDispatch } from '@/services/store.ts'
 
 export const DecksPagination = (props: { pagination: Pagination }) => {
   const dispatch = useAppDispatch()
-  const { itemsPerPage, totalItems, totalPages, currentPage } = props.pagination
+  const { itemsPerPage, totalPages, currentPage } = props.pagination
+  const changeCurrentPage = (_event: React.ChangeEvent<unknown>, currentPage: number) => {
+    dispatch(decksActions.setCurrentPage({ currentPage }))
+  }
+  const changeItemsPerPage = (itemsPerPage: number | string) => {
+    typeof itemsPerPage === 'number' && dispatch(decksActions.setItemsPerPage({ itemsPerPage }))
+  }
 
   return (
     <Wrapper>
-      <MyPagination count={totalPages} />
+      <MyPagination count={totalPages} page={currentPage} onChange={changeCurrentPage} />
       <Typography as={'body2'}>Show</Typography>
-      <Select arr={[10, 20, 30, 50, 100]} value={itemsPerPage} />
+      <Select arr={[10, 15, 20, 30, 50]} value={itemsPerPage} onChange={changeItemsPerPage} />
       <Typography as={'body2'}> on page</Typography>
     </Wrapper>
   )
